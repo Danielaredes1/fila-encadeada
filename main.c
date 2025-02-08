06a5854fd271b09937241e3dc360abbd5208488c
Danielaredes1 <161955423+Danielaredes1@users.noreply.github.com>
2025-02-08 17:44:31 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,3 +2,32 @@
 Matrícula: 20242005840
 Beatriz Piedade Rodrigues
 Matrícula: 20242005699 */
+
+/* Daniela Souza Costa 
+Matrícula: 20242005840
+Beatriz Piedade Rodrigues
+Matrícula: 20242005699 */
+
+#include "fila.h"
+
+int main(){
+    while(1){
+        int n;
+        printf("Operações:\n(1) para empilhar;\n(2) para desempilhar;\n(3) para salvar\n(4) para fechar.");
+        scanf("%d", &n);
+
+        switch (n) {
+            case 1:
+
+                break;
+        
+            default:
+                printf("Operação inválida");
+                break;
+        }
+    }
+
+    return 0;
+}
+
+
