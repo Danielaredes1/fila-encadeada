6b5d3a630809e03dee6bc4c903201d950bdbea4b
Danielaredes1 <161955423+Danielaredes1@users.noreply.github.com>
2025-02-08 16:44:15 -0300

    Update fila.h

--- fila.h
+++ fila.h
@@ -1,4 +1,18 @@
 /* Daniela Souza Costa 
 Matrícula: 20242005840
 Beatriz Piedade Rodrigues
-Matrícula: 20242005699 *\
+Matrícula: 20242005699 */
+
+#ifndef FILA_H
+#define FILA_H
+
+int criaFila();
+int testeVazia();
+void enfileirar();
+void desenfileirar();
+void desalocar();
+void imprimir();
+void lerArquivo();
+void salvarArquivo();
+
+#endif
