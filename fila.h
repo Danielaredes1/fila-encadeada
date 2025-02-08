56015da67cf89f035c43ba1eed780692b4e2735e
Danielaredes1 <161955423+Danielaredes1@users.noreply.github.com>
2025-02-08 17:22:44 -0300

    Update fila.h

--- fila.h
+++ fila.h
@@ -6,9 +6,27 @@ Matrícula: 20242005699 */
 #ifndef FILA_H
 #define FILA_H
 
-int criaFila();
-int testeVazia();
-void enfileirar();
+#include <stdio.h> 
+#include <stdlib.h>
+
+typedef struct dados_{
+    char nome[100];
+    int idade;
+    char descricao[1000];
+    char extra1[50];
+    char extra2;
+    struct dados_ *prox;
+
+} dados;
+
+typedef struct{
+    dados *inicio;
+    dados *fim;
+} fila;
+
+void criaFila(fila *cria);
+int testeVazia(fila *fila);
+void enfileirar(fila *fila, dados d);
 void desenfileirar();
 void desalocar();
 void imprimir();
