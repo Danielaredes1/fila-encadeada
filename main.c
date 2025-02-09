bd437c9636ed46aa5a8d068121a7c830efe27d5d
Danielaredes1 <161955423+Danielaredes1@users.noreply.github.com>
2025-02-09 19:25:25 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,28 +3,51 @@ Matrícula: 20242005840
 Beatriz Piedade Rodrigues
 Matrícula: 20242005699 */
 
-/* Daniela Souza Costa 
-Matrícula: 20242005840
-Beatriz Piedade Rodrigues
-Matrícula: 20242005699 */
-
 #include "fila.h"
 
 int main(){
+    fila *minhaFila = criaFila();
+    dados *novoDado;
+
     while(1){
-        fila *minhaFila = criaFila();
         int n;
-        lerDeArquivo(minhaFila);
+        lerArquivo(minhaFila);
         
         printf("Operações:\n(1) para empilhar;\n(2) para desempilhar;\n(3) para salvar\n(4) para fechar.\n");
         scanf("%d", &n);
+        if(n == 4){
+            esvaziar(minhaFila);
+            desalocar(minhaFila);
+            break;
+        }
 
         switch (n) {
             case 1:
-                dados novoDado;
+                novoDado = (dados *)malloc(sizeof(dados));
+
                 printf("digite o nome:\n"); getchar();
-                fgets(novoDado.nome, 100, stdin);
+                fgets(novoDado->nome, 100, stdin);
+
+                printf("digite a ideade:\n"); getchar();
+                fgets(novoDado->idade, 10, stdin);
+
+                printf("Informe a descrição:\n"); getchar();
+                fgets(novoDado->descricao, 100, stdin);
+
+                printf("Dado opcional:\n"); getchar();
+                fgets(novoDado->extra1, 50, stdin);
+
+                printf("Dado opcional:\n"); getchar();
+                fgets(novoDado->extra2, 50, stdin);
+
+                enfileirar(minhaFila, *novoDado);
                 break;
+
+            case 2:
+                desenfileirar(minhaFila);
+                break;
+            case 3:
+                salvarArquivo(minhaFila);
         
             default:
                 printf("Operação inválida");
