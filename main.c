84c86178f076c5cc0d76dd477c960bfbc8fbe428
bia-rodriguess <beatrizpiedade1@gmail.com>
2025-02-09 15:32:03 -0300

    Update main.c

--- main.c
+++ main.c
@@ -12,13 +12,18 @@ Matrícula: 20242005699 */
 
 int main(){
     while(1){
+        fila *minhaFila = criaFila();
         int n;
+        lerDeArquivo(minhaFila);
+        
         printf("Operações:\n(1) para empilhar;\n(2) para desempilhar;\n(3) para salvar\n(4) para fechar.\n");
         scanf("%d", &n);
 
         switch (n) {
             case 1:
-
+                dados novoDado;
+                printf("digite o nome:\n"); getchar();
+                fgets(novoDado.nome, 100, stdin);
                 break;
         
             default:
